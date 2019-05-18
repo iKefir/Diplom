9d8f340e40117bf2aa1129bb75aa9cec51136fb5
Danil Shkarupin <danil.shkarupin@corp.mail.ru>
2019-05-18 04:16:54 +0300

    Added necessary files

---
+++ config/assembled.c
@@ -0,0 +1,16 @@
+[suite]
+suite_name = PBO
+functions_id = 2
+instances_id = 1
+dimensions = 100
+[observer]
+observer_name = PBO
+result_folder = fit_bi_5000_stat
+algorithm_name = fit_bi_5000_stat
+algorithm_info = fit_bi_5000_stat
+parameters_name = best_value,mutation_rate
+[triggers]
+number_target_triggers = 3
+base_evaluation_triggers = 1,2,5
+complete_triggers = false
+number_interval_triggers = 10
