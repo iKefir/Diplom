38650eef2c9e45c6208ec5eb5a6fa8778814f7b7
Danil Shkarupin <danshk@mail.ru>
2020-03-27 10:21:08 +0300

    Implemented true multithreading with progress

--- 1_experiment/execute_bunch_of_experiments.sh
+++ 1_experiment/execute_bunch_of_experiments.sh
@@ -17,64 +17,149 @@
 # algorithm_defines = ab | resend
 #   ab     - uses ab mutation rate change
 
+if [ "$#" -eq 0 ]; then
+  echo >&2 "Missing experiment name argument"
+  exit 1
+fi
+
 DIR=$(dirname $0)
 
 newpath=${DIR}/../../1Diplom/1_experiment/RunResults/$1
 
 worker_amount=${2:-1}
 
-dimension=100
-restarts=100
-budget_multiplier=6
+pipe=/tmp/experimentPipe
+processing_info_pipe=/tmp/processingInfoPipe
 
-func_id_arr=(1 4 7 9 2 11 14 16) # 1 4 7 9 2 11 14 16
-ua_arr=(stat ab)
-fitness_arr=(bi pm)
-frequency_arr=(5000 500 50 5)
+cleanup(){
+  rm -f $pipe
+  rm -f $processing_info_pipe
+}
 
-full_counter=$((${#func_id_arr[@]}*${#ua_arr[@]}*${#fitness_arr[@]}*${#frequency_arr[@]}+${#func_id_arr[@]}*${#ua_arr[@]}))
+trap "cleanup; exit" INT TERM
+trap "cleanup; kill 0" EXIT
 
-counter=0
+if [[ ! -p $pipe ]]; then
+  mkfifo $pipe
+fi
+
+if [[ ! -p $processing_info_pipe ]]; then
+  mkfifo $processing_info_pipe
+fi
 
 workers=()
-for (( worker_i=0; worker_i<$worker_amount; worker_i++ )); do
-  workers+=( "${DIR}/../../$((worker_i+1))Diplom/1_experiment/execute_one_experiment.sh" )
-done
+worker_last_info=()
+finished_workers=0
 jobs=()
-for func_id in ${func_id_arr[@]}; do
-for ua in ${ua_arr[@]}; do
-  for fitness in stat; do
-    for frequency in 0; do
-      jobs+=( "${fitness} ${frequency} ${ua} ${dimension} ${restarts} ${newpath}/${func_id} ${func_id} ${budget_multiplier}" )
-    done
-  done
+job_i=0
 
-  for fitness in ${fitness_arr[@]}; do # bi pm
-    for frequency in ${frequency_arr[@]}; do # 1 5 10 50 100 500 1000 5000 10000 20000 100000
-      jobs+=( "${fitness} ${frequency} ${ua} ${dimension} ${restarts} ${newpath}/${func_id} ${func_id} ${budget_multiplier}" )
+setup_workers(){
+  for (( worker_i=0; worker_i<$worker_amount; worker_i++ )); do
+  workers+=( "${DIR}/../../$((worker_i+1))Diplom/1_experiment/execute_one_experiment.sh" )
+  worker_last_info+=( "WAITING" )
+  done
+}
+
+generate_jobs(){
+  dimension=100
+  restarts=100
+  budget_multiplier=6
+
+  func_id_arr=(1 4 7 9 2 11 14 16) # 1 4 7 9 2 11 14 16
+  ua_arr=(stat)
+  fitness_arr=(bi)
+  frequency_arr=(5000 500)
+
+  for func_id in ${func_id_arr[@]}; do
+    for ua in ${ua_arr[@]}; do
+      for fitness in stat; do
+        for frequency in 0; do
+          jobs+=( "${fitness} ${frequency} ${ua} ${dimension} ${restarts} ${newpath}/${func_id} ${func_id} ${budget_multiplier}" )
+        done
+      done
+
+      for fitness in ${fitness_arr[@]}; do
+        for frequency in ${frequency_arr[@]}; do
+          jobs+=( "${fitness} ${frequency} ${ua} ${dimension} ${restarts} ${newpath}/${func_id} ${func_id} ${budget_multiplier}" )
+        done
+      done
     done
   done
-done
-done
+}
+
+print_processing_info(){
+  job_i=0
+  while true; do
+    if read line <$processing_info_pipe; then
+      # printf "READ: $line\n"
+      if [[ "$line" == 'tinc' ]]; then
+        ((job_i++))
+        continue
+      fi
+      if [[ "$line" == 'quit' ]]; then
+        break
+      fi
+      IFS=',' read -r -a args <<< "$line"
+      worker_last_info[${args[0]}]="${args[1]}"
+      to_print="\r\033[K\033[1F\033[K"
+      for (( worker_i=0; worker_i<${#workers[@]}; worker_i++ )); do
+        to_print+="\033[1F\033[K"
+      done
+      to_print+="$((job_i*100/ $1))%%\t${job_i} / $1\tExperimenting\n"
+      for (( worker_i=0; worker_i<${#workers[@]}; worker_i++ )); do
+        to_print+="WORKER\t$((worker_i+1))\t${worker_last_info[$worker_i]}\n"
+      done
+      printf "$to_print"
+    fi
+  done
+}
+
+update_processing_info(){
+  # printf "WRITE: $1,$2\n"
+  echo "$1,$2" >$processing_info_pipe
+}
+
+start_worker(){
+  job=${jobs[$2]}
+  worker=${workers[$1]}
+  # printf "Worker: $worker\tJob: $job\n"
+  if [ -n "$job" ]; then
+    $worker $job | while read line; do update_processing_info $1 "$line"; done
+    echo "tinc" >$processing_info_pipe
+    sleep 1
+    echo "$1" >$pipe
+  else
+    echo "quit" >$pipe
+  fi
+}
+
+setup_workers
+generate_jobs
 
 printf "\n"
 for (( worker_i=0; worker_i<${#workers[@]}; worker_i++ )); do
   printf "\n"
 done
 
-for (( job_i=0; job_i<${#jobs[@]}; job_i+=${#workers[@]} )); do
-  printf "\r\033[K\033[1F\033[K"
-  for (( worker_i=0; worker_i<${#workers[@]}; worker_i++ )); do
-    printf "\033[1F\033[K"
-  done
-  printf "$((counter*100/full_counter))%%\t$counter / $full_counter\tExperimenting\n"
-  for (( worker_i=0; worker_i<${#workers[@]}; worker_i++ )); do
-    job=${jobs[$(( $job_i + $worker_i ))]}
-    worker=${workers[$worker_i]}
-    if [ -n "$job" ]; then
-      ((counter++))
-      $worker $job &
+print_processing_info ${#jobs[@]} &
+for (( worker_i=0; worker_i<${#workers[@]}; worker_i++ )); do
+  start_worker $worker_i $job_i &
+  ((job_i++))
+  sleep 1
+done
+while true; do
+  if [[ ! -p $pipe ]]; then
+    break
+  fi
+  if read line <$pipe; then
+    if [[ "$line" == 'quit' ]]; then
+      ((finished_workers++))
+      if [ $finished_workers -eq ${#workers[@]} ]; then
+        exit
+      fi
+      continue
     fi
-  done
-  wait
+    start_worker $line $job_i &
+    ((job_i++))
+  fi
 done
