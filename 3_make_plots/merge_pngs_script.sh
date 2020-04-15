a8830f15936d89e41ecc8ceeec64085a3eb934ad
Danil Shkarupin <danshk@mail.ru>
2020-04-15 18:47:13 +0300

    Finally good experiments with kappa

--- 3_make_plots/merge_pngs_script.sh
+++ 3_make_plots/merge_pngs_script.sh
@@ -41,8 +41,10 @@ mkdir ${p}/${graph_type}/OM/merged
 bi_arr=("fit_stat_0" "fit_bi_100" "fit_bi_50")
 dyn_arr=("_bi_1" "_bi_2" "_bi_3" "_bi_4" "_bi_5" "_pm")
 freq_arr=("_5000" "_500" "_50" "_5")
-# dyn_arr=("_bi_1_5" "_bi_1_50" "_bi_5_5" "_bi_5_50")
-# gamma_arr=("1_" "3_" "5_" "10_")
+
+dyn_arr=("_bi_1_5" "_bi_1_50" "_bi_1_500" "_bi_5_5" "_bi_5_50" "_bi_5_500")
+gamma_arr=("1_" "3_" "5_" "10_")
+
 fun_ids=(2)
 n_bi_arr=()
 n_pm_arr=()
@@ -60,23 +62,23 @@ for fun_id in ${fun_ids[@]}; do
 done
 
 n_arr=()
-for dyn in ${dyn_arr[@]}; do
-    n_arr+=("${p}/2/${s_p}/fit_stat_0.png")
-    for fr in ${freq_arr[@]}; do
-        n_arr+=("${p}/2/${s_p}/fit${dyn}${fr}.png")
-    done
-done
-# for gm in ${gamma_arr[@]}; do
-#     for dyn in ${dyn_arr[@]}; do
-#         n_arr+=("${p}/2/${s_p}/${gm}fit${dyn}.png")
+# for dyn in ${dyn_arr[@]}; do
+#     n_arr+=("${p}/2/${s_p}/fit_stat_0.png")
+#     for fr in ${freq_arr[@]}; do
+#         n_arr+=("${p}/2/${s_p}/fit${dyn}${fr}.png")
 #     done
 # done
+for gm in ${gamma_arr[@]}; do
+    for dyn in ${dyn_arr[@]}; do
+        n_arr+=("${p}/2/${s_p}/${gm}fit${dyn}.png")
+    done
+done
 
 mkdir ${p}/${graph_type}/LO
 mkdir ${p}/${graph_type}/LO/merged
 # montage -geometry 1280x960 -tile ${#bi_arr[@]}x${#fun_ids[@]} ${n_bi_arr[@]} ${p}/${graph_type}/LO/merged/LO_bi_${graph_type}.png
 # montage -geometry 1280x960 -tile ${#pm_arr[@]}x${#fun_ids[@]} ${n_pm_arr[@]} ${p}/${graph_type}/LO/merged/LO_pm_${graph_type}.png
-montage -geometry 1280x1440 -tile 5x6 ${n_arr[@]} ${p}/${graph_type}/LO/merged/LO_${graph_type}.png
-# montage -geometry 1280x1440 -tile 4x4 ${n_arr[@]} ${p}/${graph_type}/LO/merged/LO_${graph_type}.png
+# montage -geometry 1280x1440 -tile 5x6 ${n_arr[@]} ${p}/${graph_type}/LO/merged/LO_${graph_type}.png
+montage -geometry 1280x1440 -tile 6x4 ${n_arr[@]} ${p}/${graph_type}/LO/merged/LO_${graph_type}.png
 
 done
